0ce48f2fc34cb9092bf86b0f7b75d483994ab5a6
Francisco lucas (zeta) <lucasnasm@gmail.com>
2019-08-07 08:44:21 -0300

    alteracao - usando .env no linux :fire:

--- src/server.js
+++ src/server.js
@@ -1,15 +1,23 @@
+//carregar variaveis ambiente - não expor minhas configs do db no git
+const dotenv = require('dotenv')
 const express = require('express')
+const mongoose = require('mongoose')
+dotenv.config()
+//import de arquivo na mesma pasta
+const routes = require('./routes')
 
 //instancia do express
 const server = express();
 
-//metodo get com arrow function
-server.get('/', (req,res) => {
-  return res.json({message: `OLá ${req.query.name}`})
-})
 
+//fazendo com que o express entenda json na req
+server.use(express.json())
 
+//conexão com o mongodb
+mongoose.connect(process.env.DB_CON,{useNewUrlParser:true})
 
+//carregando rotas
+server.use(routes)
 
 //ouvindo na porta 3000
-server.listen(3000)
+server.listen(process.env.PORT)
